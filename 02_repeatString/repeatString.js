007b983d75664847c37c5c0d1cba58804c638c8b
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-04-14 18:30:13 +0200

    tested my first idea -> failed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const stg = 'hey'
+const num = 0
 
+const repeatString = function() {
+    for (; num > 3; num++){
+        repeatString(stg);
+    }
 };
 
 // Do not edit below this line
