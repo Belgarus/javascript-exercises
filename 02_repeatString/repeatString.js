b0b4db7a2ac7cd6834d1c28245ad6f603cb956e6
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-04-15 18:22:35 +0200

    passed the 6th test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,11 @@
-const stg = 'goodbye'
-const num = -1
+const stg = 'hey'
+const num = Math.floor((Math.random()*1000)+1)
 
 const repeatString = function(stg, num) {
     let result = "";
     if (num < 0) return 'ERROR'
     for (let i = 0; i < num; i++){
-        result += 'goodbye'
+        result += stg
     }
     return result
 };
