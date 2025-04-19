acdcda1fac07a26813f8a517b95179ec889712a7
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-04-19 17:59:19 +0200

    solved the second exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,7 +2,7 @@ const removeFromArray = function() {
     let Array = [1, 2, 3, 4];
     let newArray = []
     for (let i = 0; i < Array.length; i++){
-            if(i != 2){
+            if(i !== 1 && i !== 2){
                 newArray.push(Array[i])
             }
     }
