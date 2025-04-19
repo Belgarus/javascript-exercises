83c0255ffcfcecc2573838be7dee4cb1f9e8a363
Belgarus <Noahskiera77@gmail.com>
2025-04-19 14:17:52 +0200

    added console.log

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,12 +1,15 @@
-let str = "hello there"
+let str = "hello there";
 
 const reverseString = function(str) {
-        let newStr = ""
-        for (let i = str.length - 1; i >= 0; i--){
-             newStr += str[i];
-            }
-return newStr
+  let newStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
 };
+
+// Ausgabe ins Terminal
+console.log(reverseString(str));
 
 // Do not edit below this line
 module.exports = reverseString;
