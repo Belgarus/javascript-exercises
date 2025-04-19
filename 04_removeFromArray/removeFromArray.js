2fe46c7a805189093acddce53e8553085b04ebdd
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-04-19 17:38:23 +0200

    found solution to the first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,15 @@
 const removeFromArray = function() {
-    Array = [1, 2, 3, 4];
-
+    let Array = [1, 2, 3, 4];
+    let newArray = []
+    for (let i = 0; i < Array.length; i++){
+            if(i != 2){
+                newArray.push(Array[i])
+            }
+    }
+    return newArray;
 };
 
+
+console.log(removeFromArray())
 // Do not edit below this line
 module.exports = removeFromArray;
